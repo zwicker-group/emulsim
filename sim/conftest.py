489f0fa8865629ede685e822aeb0b2991c25f6b0
David Zwicker <david.zwicker@ds.mpg.de>
2020-11-25 17:25:59 +0100

    Tests (#2)
    * Improved test infrastructure
    
    * Updated build script

--- sim/conftest.py
+++ sim/conftest.py
@@ -4,15 +4,22 @@ This file is used to configure the test environment when running py.test
 .. codeauthor:: David Zwicker <david.zwicker@ds.mpg.de>
 """
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pytest
 
 
-@pytest.fixture(scope="session", autouse=True)
-def enable_raise_errors():
-    """ helper function enabling errors for all tests """
-    # prepare something ahead of all tests
+@pytest.fixture(scope="function", autouse=True)
+def suppressing():
+    """ helper function adjusting message reporting for all tests """
+    # raise all underflow errors
     np.seterr(all="raise", under="ignore")
+
+    # run the actual test
+    yield
+
+    # clean up open matplotlib figures after the test
+    plt.close("all")
 
 
 def pytest_addoption(parser):
